00c927cc81aac0251ea020d6e10f08cffdde789f
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-02-12 18:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let btn = document.getElementById('btn');
+let commentSection = document.getElementById('commentSection');
+let commentShowTab = document.getElementById('commentShowingTab');
+let name;
+let date;
+let dateSorting = [];
+
+btn.addEventListener("click", function(){
+	if(commentSection === '')
+	{
+		alert("Comment please");
+	}
+		
+	else
+	{ name = prompt("Enter Name ");
+	 date = prompt("Enter Date");
+	 dateSorting.push(date);
+	commentShowTab.innerHTML = commentSection.value;
+	const underline = document.createElement("U");
+	commentShowTab.appendChild(underline);
+     commentSection.value = "";
+	commentShowTab.style.display = 'block';
+	}
+	
+})
